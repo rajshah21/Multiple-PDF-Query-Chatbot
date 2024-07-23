86e0d16e254ae87822d854bbc3eaead235718399
Raj Shah <59496726+rajshah21@users.noreply.github.com>
2024-07-23 11:59:54 -0500

    Multiple PDF Query Chatbot

---
+++ app2.py
@@ -0,0 +1,163 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import HumanMessage
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from htmlTemplates import css, bot_template, user_template
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator = '\n',
+        chunk_size = 1000,
+        chunk_overlap=200,
+        length_function = len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    vectorstore.save_local("faiss_index")
+    return vectorstore
+    
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+
+    contextualize_q_system_prompt = """Given a chat history and the latest user question \
+    which might reference context in the chat history, formulate a standalone question \
+    which can be understood without the chat history. Do NOT answer the question, \
+    just reformulate it if needed and otherwise return it as is."""
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    history_aware_retriever = create_history_aware_retriever(
+        llm, vectorstore.as_retriever(), contextualize_q_prompt
+    )
+
+    ### Answer question ###
+    qa_system_prompt = """You are an assistant for question-answering tasks. \
+    Use the following pieces of retrieved context to answer the question. \
+    If you don't know the answer, just say that you don't know. \
+
+    {context}"""
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", qa_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+    
+    ### Statefully manage chat history ###
+    store = {}
+
+
+    def get_session_history(session_id: str) -> BaseChatMessageHistory:
+        if session_id not in store:
+            store[session_id] = ChatMessageHistory()
+        return store[session_id]
+
+
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+        output_messages_key="answer",
+    )
+
+    return conversational_rag_chain
+
+
+def handle_userinput(user_question,chat_history):
+    response = st.session_state.conversation.invoke(
+        {"input":user_question, "chat_history":chat_history},
+        config={"configurable": {"session_id": "abc123"}
+        },
+    )
+    chat_history.extend([HumanMessage(content=user_question),response['answer']])
+    st.write(bot_template.replace(
+                "{{MSG}}", chat_history[1]), unsafe_allow_html=True)
+    st.write(user_template.replace(
+                "{{MSG}}", chat_history[0].content), unsafe_allow_html=True)
+    
+    for i,message in reversed(list(enumerate(response['chat_history']))):
+        if i%2==0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+    
+    
+
+
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title='Chat with Multiple PDFs', page_icon=':books:')
+
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    chat_history=[]
+
+    st.header("Chat with Multiple PDFs :books:")
+    user_question = st.text_input('Ask any Question about your PDFs:')
+    if user_question:
+        handle_userinput(user_question,chat_history)
+
+
+    with st.sidebar:
+        st.subheader('My Documents:')
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                #Get PDFs Text
+                raw_text = get_pdf_text(pdf_docs)
+
+                #Get Text Chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                #Create Vector Store
+                vectorstore = get_vectorstore(text_chunks)
+
+                #Get Conversational chain
+                st.session_state.conversation = get_conversation_chain(vectorstore)
+
+
+
+
+if __name__=='__main__':
+    main()
