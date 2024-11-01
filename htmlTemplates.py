90250b277ea1420cea4a7ef015d1f77e0f876e47
Raj Shah <59496726+rajshah21@users.noreply.github.com>
2024-11-01 18:21:45 -0500

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -28,7 +28,7 @@ css = '''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://ibb.co/LhRnNH5">
+        <img src="Cartoon Style Robot.jpg">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
@@ -37,7 +37,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://ibb.co/gwFxmxV">
+        <img src="49584.jpg">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
